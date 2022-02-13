bdaa41dafca103c42020e4da10f6084686b0442c
Jacob Zelko <jacobszelko@gmail.com>
2022-02-13 15:50:24 -0500

    Removed initial demos of bookings and movies

--- app/resources/bookings/views/bookings.jl
+++
@@ -1,9 +0,0 @@
-Html.div([
-heading("Book your movie here:")
-
-page(
-  model, partial = true, [
-    row([
-      ])
-    ])
-])
--- app/resources/movies/views/movies.jl
+++
@@ -1,9 +0,0 @@
-Html.div([
-heading("Explore available movies here:")
-
-page(
-  model, partial = true, [
-    row([
-      ])
-    ])
-])
